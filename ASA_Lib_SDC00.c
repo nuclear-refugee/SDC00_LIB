7ff35371559010d3a5019ada066aace1d37e6f3e
TwnET <a0919958057@gmail.com>
2017-09-28 10:39:53 +0800

    Finish ASA_SDC00_get() & ASA_SDC00_set()

--- ASA_Lib_SDC00.c
+++ ASA_Lib_SDC00.c
@@ -6,41 +6,60 @@
 #include "ASA_Core_M128.h"
 #include "ff.h"
 
+#define _DEBUG_INFO
+
 /**** Internal Definitions ****/
 
 // SDC00 set File Control Flag:
-#define SDC_FCF_CLOSE			0x01
-#define SDC_FCF_READ			0x02
-#define SDC_FCF_OVERWRITE		0x04
-#define SDC_FCF_CONTINUEWRITE	0x08
-#define SDC_FCF_READPARAMETER	0x10
-#define SDC_FCF_RENAME			0x20
+#define SDC_FCF_CLOSE			0x00
+#define SDC_FCF_READ			0x01
+#define SDC_FCF_OVERWRITE		0x05
+#define SDC_FCF_CONTINUEWRITE	0x07
+//#define SDC_FCF_READPARAMETER		0x10
+//#define SDC_FCF_RENAME			0x20
 
 // ASA SDC Return Error Code Table
-#define ARSDC_OK 0
-#define ARSDC_ID_ERR 1
-#define ARSDC_LSBYTE_ERR 2
-#define ARSDC_SHIFT_ERR 3
-#define ARSDC_MUTI_SET_ERR 4
-#define ARSDC_FILE_ID_ERR 5
-#define ARSDC_OPEN_FILE_ERR 6
-#define ARSDC_SDC_STATE_ERR 7
-#define ARSDC_BYTES_ERR 8
-#define ARSDC_NO_OPEN_FILE 9
-#define ARSDC_READ_SETTING 10
-#define ARSDC_LIST_READ_ONLY 11
-#define ARSDC_WRITE_SETTING 12
-#define ARSDC_NO_DATA_TO_READ 13
-#define ARSDC_DATA_NUM_ENOUGH 14
-#define ARSDC_RENAME_ERR 15			// 指定檔案編號不存在或檔名已存在不能重複使用
+#define ARSDC_OK				0
+#define ARSDC_ID_ERR			1
+#define ARSDC_LSBYTE_ERR		2
+#define ARSDC_SHIFT_ERR			3
+#define ARSDC_MUTI_SET_ERR		4
+#define ARSDC_FILE_ID_ERR		5
+#define ARSDC_OPEN_FILE_ERR		6
+#define ARSDC_SDC_STATE_ERR		7
+#define ARSDC_BYTES_ERR			8
+#define ARSDC_NO_OPEN_FILE		9
+#define ARSDC_READ_SETTING		10
+#define ARSDC_LIST_READ_ONLY	11
+#define ARSDC_WRITE_SETTING		12
+#define ARSDC_NO_DATA_TO_READ	13
+#define ARSDC_DATA_NUM_ENOUGH	14
+#define ARSDC_RENAME_ERR		15			// 指定檔案編號不存在或檔名已存在不能重複使用
 
-// SDC00 set File Control Flag:
-#define SDC_FCF_CLOSE			0x01
-#define SDC_FCF_READ			0x02
-#define SDC_FCF_OVERWRITE		0x04
-#define SDC_FCF_CONTINUEWRITE	0x08
-#define SDC_FCF_READPARAMETER	0x10
-#define SDC_FCF_RENAME			0x20
+/**** SDC00 LSByte definition ****/
+// Register address
+#define LSBYTE_DATA_SWAP_BUF	0
+#define LSBYTE_NAME_BUF			64
+#define LSBYTE_NAME_EXT_BUF		72
+#define LSBYTE_ATTR_ACCESS		75
+#define LSBYTE_ATTR_TIME		76
+#define LSBYTE_ATTR_DATE		78
+#define LSBYTE_CLUSTER			80
+#define LSBYTE_FILE_SZ			82
+#define LSBYTE_FILE_SERIAL_ID	86
+#define LSBYTE_MODE				200
+
+// Register size
+#define SZ_DATA_SWAP_BUF		64
+#define SZ_NAME_BUF				8
+#define SZ_NAME_EXT_BUF			3
+#define SZ_ATTR_ACCESS			1
+#define SZ_ATTR_TIME			2
+#define SZ_ATTR_DATE			2
+#define SZ_CLUSTER				2
+#define SZ_FILE_SZ				4
+#define SZ_FILE_SERIAL_ID		2
+#define SZ_FILE_MODE			1
 
 /**** Internal Function Prototypes ****/
 
@@ -52,7 +71,7 @@ FILINFO Finfo;
 FIL FileObj;				// File object
 DIR DirObj;					// Directory object
 
-char FileName[13] = "DAT00000.BIN";
+char FileName[13];
 char ASA_DATA_Dir[9] = "ASA_DATA";
 unsigned int FileID = 0;
 char SDC_State = 0;
@@ -64,7 +83,7 @@ unsigned long EndPointOfFile = 0;
 unsigned int LastFileIndex = 0;
 
 //Functions
-
+FRESULT check_info(char* file_name, FILINFO* p_fno);
 char ASA_ID_check(unsigned char ASA_ID)
 {
 	if(ASA_ID>=0 && ASA_ID<=7)
@@ -149,23 +168,24 @@ char ASA_SDC00_Init(char ASA_ID)
 {
 	char res;	// 0:All OK, 1:SD Not OK, 2:RTC Not OK, 3:All Not OK.
 	char i;
+
 	// TODO: Add RTC Library and enable these code
 	//// 先檢查是否有RTC00可供配合提供時間
 	//if( RTC_Init_Success_Flag == 0 )			// 第一次被呼叫設定時進行RTC初始化
 	//{
-		//for(i=0; i<8; i++)						// 跑一趟for loop自動偵測RTC ID編號
-		//{
-			//ASA_ID_set(i);
-			//if( RTC_init() == 1 )
-			//{
-				//RTC_Init_Success_Flag = 1;
-				//ASA_RTC00_ID = i;
-				//break;
-			//}
-		//}
-		////	if( ASA_RTC00_ID == 44 )
-		////		res = 2;
-		////return ARRTC_INVALID_ERR;
+	//for(i=0; i<8; i++)						// 跑一趟for loop自動偵測RTC ID編號
+	//{
+	//ASA_ID_set(i);
+	//if( RTC_init() == 1 )
+	//{
+	//RTC_Init_Success_Flag = 1;
+	//ASA_RTC00_ID = i;
+	//break;
+	//}
+	//}
+	////	if( ASA_RTC00_ID == 44 )
+	////		res = 2;
+	////return ARRTC_INVALID_ERR;
 	//}
 
 
@@ -174,18 +194,24 @@ char ASA_SDC00_Init(char ASA_ID)
 	_delay_ms(100);
 
 	res = 0;
+#ifdef _DEBUG_INFO
 	printf("Set ASA ID :%d \n", ASA_ID);
+#endif
 	ASA_ID_set(ASA_ID);
-		
+	
 	if( f_mount(&FatFs[0], "", 1) != FR_OK )			// SD card mount a volume
 	{
+#ifdef _DEBUG_INFO
 		printf("Mount ID<%d> Failed\n", ASA_ID);
+#endif
 		res = 1;
 		return res;
 	}
+#ifdef _DEBUG_INFO
 	else {
 		printf("Mount ID<%d> successful\n", ASA_ID);
 	}
+#endif
 
 	if( f_chdir(ASA_DATA_Dir) != FR_OK )				// 切換路徑至ASA預設資料夾
 	{
@@ -211,7 +237,9 @@ char ASA_SDC00_Init(char ASA_ID)
 	}
 
 	ASA_SDC00_Deselect();
-
+#ifdef _DEBUG_INFO
+	printf("<SDC00_Init> Initialize with result : %d \n", res);
+#endif
 	return res;
 }
 
@@ -223,14 +251,14 @@ char FileIDtoNameConvert(int ID, char* Name)
 	while(1)
 	{
 		if( f_readdir(&DirObj, &Finfo) != FR_OK )	// SD讀寫通訊異常
-			return 2;
+		return 2;
 		
 		if( !Finfo.fname[0] )						// 若fname為空代表已經沒有檔案可讀參
-			return 3;
+		return 3;
 		if(Finfo.fattrib & AM_DIR)
-			;
+		;
 		else if(Finfo.fattrib & AM_RDO || Finfo.fattrib & AM_HID || Finfo.fattrib & AM_SYS)
-			;
+		;
 		else {
 			if( obj_index == ID )
 			{
@@ -248,116 +276,59 @@ char ASA_SDC00_set(char ASA_ID, char LSByte, char Mask, char shift, char Data)
 {
 	char set_Data = 0, SysInfoIDTemp[2];
 	unsigned int BytesTemp;
-	if( SDC_Init_Success_Flag == 0 )			// 第一次被呼叫設定時進行SDC初始化
+	if( SDC_Init_Success_Flag == 0 )	// Initialize at first call
 	{
 		if( ASA_SDC00_Init(ASA_ID) == 0 )
 		SDC_Init_Success_Flag = 1;
 		else
 		return ARSDC_SDC_STATE_ERR;
 	}
-	if( ASA_SDC00_ID != ASA_ID )	// ASA_ID錯誤
+	if( ASA_SDC00_ID != ASA_ID )	// Not a valid ASA_ID
 	return ARSDC_ID_ERR;
-	if( shift > 7 || shift < 0 )		// Shift錯誤
+	if( shift > 7 || shift < 0 )	// Not a valid shift value ( 0 <= shift <= 7 )
 	return ARSDC_SHIFT_ERR;
 	set_Data = (set_Data & (~Mask)) | ((Data<<shift) & Mask);
 	ASA_SDC00_Select(ASA_ID);
 	switch(LSByte)
 	{
-		case 200:
+		case LSBYTE_MODE:
 		switch(set_Data)
 		{
-			case SDC_FCF_CLOSE:			// 關檔
-			f_close(&FileObj);
-			SDC_State = 0;
-			break;
+			case SDC_FCF_CLOSE:			// Close file
+				f_close(&FileObj);
+				SDC_State = 0;
+				break;
 			
-			case SDC_FCF_READ:			// 開檔允讀
-			if( FileIDtoNameConvert(FileID, FileName) != ARSDC_OK )
-			return ARSDC_OPEN_FILE_ERR;
-			if( f_open(&FileObj, FileName, FA_READ) != FR_OK )
-			return ARSDC_OPEN_FILE_ERR;
-			SDC_State = 1;
-			break;
+			case SDC_FCF_READ:			// Open File (Read only)
+				// TODO: need to make sure there is "FileName already setup"
+				if( f_open(&FileObj, FileName, FA_READ) != FR_OK )
+					return ARSDC_OPEN_FILE_ERR;
+				SDC_State = 1;
+				break;
 			
-			case SDC_FCF_OVERWRITE:		// 開檔允蓋寫/若檔案編號設為0則開新檔案
+			case SDC_FCF_OVERWRITE:		// 若設為開檔蓋寫							
+				if( f_open(&FileObj, FileName, FA_CREATE_ALWAYS) != FR_OK )	// 強制開新檔案蓋寫
+				return ARSDC_SDC_STATE_ERR;
+				if( f_open(&FileObj, FileName, FA_WRITE) != FR_OK )			// 開檔設為寫
+				return ARSDC_SDC_STATE_ERR;
+				SDC_State = 2;
+				break;
 			case SDC_FCF_CONTINUEWRITE:	// 開檔允續寫/若檔案編號設為0則開新檔案
-			// 若一開始就要開新檔案，則指定檔案編號ID為0(預設保留給自動命名開新檔)即可
-			if( FileIDtoNameConvert(FileID, FileName) != ARSDC_OK )				// 用ID去全部開一次路徑找出對應檔名後再開檔
-			{
-				if( FileID == 0 )												// 若指定ID為0自動開新檔案
-				{
-					// 自動產生新檔名並開新檔案
-					sprintf(FileName, "DAT%05d.BIN", LastFileIndex+1);
-					if( f_open(&FileObj, FileName, FA_CREATE_ALWAYS) != FR_OK )	// 開新檔案
-					return ARSDC_SDC_STATE_ERR;
-					f_close(&FileObj);
-
-					// 如果自動產生的新檔名開檔成功，則修改系統檔記錄之檔名流水序號
-					LastFileIndex++;
-					SysInfoIDTemp[1] = LastFileIndex/256;
-					SysInfoIDTemp[0] = LastFileIndex%256;
-					if( f_chmod("ASYSINFO.ASI", 0x06, 0xFF) )				// 解除唯讀
-					return ARSDC_SDC_STATE_ERR;
-					if( f_open(&FileObj, "ASYSINFO.ASI", FA_WRITE) )		// 開檔寫系統檔
-					return ARSDC_SDC_STATE_ERR;
-					if( f_write(&FileObj, SysInfoIDTemp, 2, &BytesTemp) )	// 修改系統檔已用流水序號
-					return ARSDC_SDC_STATE_ERR;
-					f_close(&FileObj);
-					if( f_chmod("ASYSINFO.ASI", 0x07, 0xFF) )				// 設定唯讀
-					return ARSDC_SDC_STATE_ERR;
-
-					if( f_open(&FileObj, FileName, FA_WRITE) != FR_OK )			// 開檔設為寫
-					return ARSDC_SDC_STATE_ERR;
-				}
-				else
-				return ARSDC_OPEN_FILE_ERR;
-			}
-			else
-			{
-				if( set_Data == SDC_FCF_OVERWRITE )								// 若設為開檔蓋寫
-				{
-					if( f_open(&FileObj, FileName, FA_CREATE_ALWAYS) != FR_OK )	// 強制開新檔案蓋寫
-					return ARSDC_SDC_STATE_ERR;
-					if( f_open(&FileObj, FileName, FA_WRITE) != FR_OK )			// 開檔設為寫
-					return ARSDC_SDC_STATE_ERR;
-				}
-				else	// if( set_Data == SDC_FCF_CONTINUEWRITE )				// 若設為開檔續寫
-				{
-					if( f_open(&FileObj, FileName, FA_WRITE) != FR_OK )			// 開檔設為寫
-					return ARSDC_SDC_STATE_ERR;
-					if( f_lseek(&FileObj, EndPointOfFile) != FR_OK )			// 將資料位置指標移到檔尾續寫
-					return ARSDC_SDC_STATE_ERR;
-				}
-			}
-			SDC_State = 2;
-			break;
-			
-			case SDC_FCF_READPARAMETER:		// 讀參
-			if( f_opendir(&DirObj, ASA_DATA_Dir) != FR_OK )
-			return ARSDC_SDC_STATE_ERR;
-			SDC_State = 4;
-			break;
-			
-			case SDC_FCF_RENAME:			// 改名
-			if( FileIDtoNameConvert(FileID, FileName) != ARSDC_OK )					// 用ID去全部開一次路徑找出對應檔名後再開檔
-			return ARSDC_RENAME_ERR;
-			SDC_State = 5;
-			break;
+				if( f_open(&FileObj, FileName, FA_WRITE) != FR_OK )			// 開檔設為寫
+				return ARSDC_SDC_STATE_ERR;
+				if( f_lseek(&FileObj, f_size(&FileObj)) != FR_OK )			// 將資料位置指標移到檔尾續寫
+				return ARSDC_SDC_STATE_ERR;
+				SDC_State = 2;
+				break;
 			
 			default:	// 同時設定兩個以上旗標，回應錯誤
-			return ARSDC_MUTI_SET_ERR;
-			break;
+				return ARSDC_MUTI_SET_ERR;
 		}
 		break;
-		case 201:	// 檔案編號必須先寫低Byte再寫高Byte
-		FileID = (unsigned char)set_Data;
-		break;
-		case 202:
-		FileID += (unsigned char)set_Data;
-		break;
-		default:	// LSByte錯誤，不在設定暫存器列表LSB200~202的範圍內
+		default:	// LSByte錯誤，不在設定暫存器列表LSB200~200的範圍內
 		return ARSDC_LSBYTE_ERR;
 		break;
+
 	}
 
 	ASA_SDC00_Deselect();
@@ -367,82 +338,119 @@ char ASA_SDC00_set(char ASA_ID, char LSByte, char Mask, char shift, char Data)
 
 char ASA_SDC00_put(char ASA_ID, char LSByte, char Bytes, void *Data_p)
 {
+	FRESULT res;
 	char FileNameNew[13];
 	unsigned int WriteBytes, i;
 
 	if( LSByte == 200 )						// 設定檔案控制旗標暫存器
-	return ASA_SDC00_set(ASA_ID, LSByte, 0xFF, 0x00, (char)((char*)Data_p)[0]);
-	if( LSByte == 202 || LSByte == 201 )	// 設定檔案編號暫存器
+		return ASA_SDC00_set(ASA_ID, LSByte, 0xFF, 0x00, (char)((char*)Data_p)[0]);
+	
+	if( SDC_Init_Success_Flag == 0 )	// Initialize at first call
 	{
-		if( LSByte == 201 )
-		{
-			if( Bytes == 1 )
-			FileID = (unsigned char)((char*)Data_p)[0];
-			if( Bytes == 2 )
-			{
-				FileID = (unsigned char)((char*)Data_p)[0];
-				FileID += (unsigned char)((char*)Data_p)[1];
-			}
-		}
+		if( ASA_SDC00_Init(ASA_ID) == 0 )
+		SDC_Init_Success_Flag = 1;
 		else
-		{
-			if( Bytes == 1 )
-			FileID += (unsigned char)((char*)Data_p)[1];
-			if( Bytes == 2 )
-			return ARSDC_BYTES_ERR;
-		}
-		return ARSDC_OK;
+		return ARSDC_SDC_STATE_ERR;
 	}
-	if( SDC_Init_Success_Flag == 0 )			// 若沒初始化表開檔會失敗
-	return ARSDC_SDC_STATE_ERR;
 	if( ASA_SDC00_ID != ASA_ID )
-	return ARSDC_ID_ERR;
-	if( LSByte > 1 || LSByte < 0 )		// 輸出暫存器
-	return ARSDC_LSBYTE_ERR;
+		return ARSDC_ID_ERR;
 	if( LSByte == 0 )
-	if( Bytes > 64 || Bytes < 0 )
-	return ARSDC_BYTES_ERR;
-	if( LSByte == 1 )
-	if( Bytes > 8 || Bytes < 0 )
-	return ARSDC_BYTES_ERR;
+		if( Bytes > 64 || Bytes < 0 )
+			return ARSDC_BYTES_ERR;
 
 	ASA_SDC00_Select(ASA_ID);
 	
+	static char name_buffer[8];
+	static char name_ext_buffer[3];
+#ifdef _DEBUG_INFO
+	printf("SDC state: %d\n", SDC_State);
+#endif
 	switch(SDC_State)
 	{
-		case 0:	// 關檔
-		return ARSDC_NO_OPEN_FILE;
-		break;
-		case 1:	// 開檔允讀
-		return ARSDC_READ_SETTING;
-		break;
-		case 2:	// 開檔允蓋寫/續寫
-		if( LSByte == 0 )
-		{
-			if( f_write(&FileObj, Data_p, Bytes, &WriteBytes) != FR_OK )
-			return ARSDC_SDC_STATE_ERR;
-		}
-		else
-		return ARSDC_LSBYTE_ERR;
-		break;
-		case 4:	// 讀參
-		return ARSDC_LIST_READ_ONLY;
-		break;
-		case 5:	// 改名
-		if( LSByte == 1 )
-		{
-			for(i=0; i<Bytes; i++)
-			FileNameNew[i] = ((char*)Data_p)[i];
-			sprintf(&FileNameNew[i], ".BIN");
-			if( f_rename(FileName, FileNameNew) != FR_OK )
-			return ARSDC_RENAME_ERR;
-		}
-		else
-		return ARSDC_LSBYTE_ERR;
-		break;
+		case 0:	// If current is in closed file mode
+			switch(LSByte) {
+			case LSBYTE_NAME_BUF:
+				for(int i=0; i<SZ_NAME_BUF; i++) {
+#ifdef _DEBUG_INFO
+					printf("Namebuf[%d]: %c\n", i, ((char*)Data_p)[i]);
+#endif
+					// Copy each byte until EOF or At byte 7
+					if(((char*)Data_p)[i] == '\0' || i ==(SZ_NAME_BUF-1) || i== Bytes) {
+						name_buffer[i] = '\0';
+						break;
+					}
+					name_buffer[i] = ((char*)Data_p)[i];
+				}
+				break;
+
+			// Note: End of name_ext string is reach '\0' or Byte 2
+			case LSBYTE_NAME_EXT_BUF:
+				for(int i=0; i<SZ_NAME_EXT_BUF; i++) {
+#ifdef _DEBUG_INFO
+					printf("Nameextbuf[%d]: %c\n", i, ((char*)Data_p)[i]);
+#endif
+					// Copy each byte until EOF or At byte-2
+					if(((char*)Data_p)[i] == '\0' || i==Bytes) {
+						name_ext_buffer[i] = '\0';
+						break;
+					}
+					name_ext_buffer[i] = ((char*)Data_p)[i];
+				}
+				break;
+			default:
+				return ARSDC_NO_OPEN_FILE;
+			}
+			break;
+		case 1:	// If current is at Opened file mode (Read Only)
+			return ARSDC_READ_SETTING;
+			break;
+		case 2:	// If current is at Opened file mode ([Overlap/Continue] Write mode)
+			if( LSByte == 0 ) {
+				if(  res = f_write(&FileObj, Data_p, Bytes, &WriteBytes)) {
+#ifdef _DEBUG_INFO
+					printf("<FAT Write> write fail errorcode: %d\n", res);
+					return ARSDC_SDC_STATE_ERR;
+#endif
+				}
+			}
+			else
+				return ARSDC_LSBYTE_ERR;
+			break;
 	}
 
 	ASA_SDC00_Deselect();
+
+	// While in closed file mode, then update full filename
+	if(SDC_State == 0) {
+#ifdef _DEBUG_INFO
+	printf("<Update Filename>\n");
+	printf("name_buffer = %s\n", name_buffer);
+	printf("name_ext_buf = %s\n", name_ext_buffer);
+#endif
+		// Check name_buffer actual size
+		int8_t sz_name_buffer = -1;
+		for(int i=0; i<8; i++) {
+			if(name_buffer[i] == '\0') {
+				sz_name_buffer = i;
+				break;
+			}
+		}
+
+		if(sz_name_buffer > 0) {
+			for(int i=0; i<sz_name_buffer; i++) {
+				FileName[i] = name_buffer[i];
+			}
+			FileName[sz_name_buffer] = '.';
+			for(int i=0; i<3; i++) {
+				FileName[sz_name_buffer+1+i] = name_ext_buffer[i];
+			}
+			FileName[sz_name_buffer+4] = '\0';
+#ifdef _DEBUG_INFO
+			printf("<Update FileName: %s\n", FileName);
+#endif
+		}
+		
+	}
 
 	return 0;
 }
@@ -451,81 +459,132 @@ char ASA_SDC00_get(char ASA_ID, char LSByte, char Bytes, void *Data_p)
 {
 	unsigned int ReadBytes;
 	int obj_index = 0;
+	char* p_data = Data_p;
+	FRESULT fr;
+	FILINFO fno;
 
 	if( SDC_Init_Success_Flag == 0 )			// 若沒初始化表開檔會失敗
 	return ARSDC_SDC_STATE_ERR;
 	if( ASA_SDC00_ID != ASA_ID )
 	return ARSDC_ID_ERR;
-	if( LSByte > 101 || LSByte < 100 )
-	return ARSDC_LSBYTE_ERR;
-	if( LSByte == 100 )
+	// TODO: Modify valid LSByte section
+	//if( LSByte > 101 || LSByte < 100 )
+	//return ARSDC_LSBYTE_ERR;
+	if( LSByte == LSBYTE_DATA_SWAP_BUF )
 	if( Bytes > 64 || Bytes < 0 )
-	return ARSDC_BYTES_ERR;
-	if( LSByte == 101 )
-	if( Bytes > 47 || Bytes < 0 )
 	return ARSDC_BYTES_ERR;
 
 	ASA_SDC00_Select(ASA_ID);
 
 	switch(SDC_State)
 	{
-		case 0:	// 關檔
+		case 0:	// If current is in closed file mode
 		return ARSDC_NO_OPEN_FILE;
 		break;
-		case 1:	// 開檔允讀
-		if( LSByte == 100 )
-		{
-			if( f_read(&FileObj, Data_p, Bytes, &ReadBytes) != FR_OK )
-			return ARSDC_SDC_STATE_ERR;
-			if(Bytes != ReadBytes)
-			{
-				if(ReadBytes == 0)
-				return ARSDC_NO_DATA_TO_READ;
-				else
-				{
-					((char*)Data_p)[(int)Bytes-1] = ReadBytes;		// 讀取資料不足設定的數量，將真實讀出數量填入資料緩衝區最後一個byte
+		case 1:	// If current is at Opened file mode (Read Only)
+			// Fetch file information first
+			if(check_info(FileName, &fno) != FR_OK) {
+				return ARSDC_OPEN_FILE_ERR;
+			}
+
+			switch(LSByte) {
+			case LSBYTE_DATA_SWAP_BUF:
+				if(f_read(&FileObj, p_data, Bytes, &ReadBytes) != FR_OK)
+					return ARSDC_SDC_STATE_ERR;
+				if(Bytes != ReadBytes) {
+					return ARSDC_NO_DATA_TO_READ;
+				}
+				else {												// While read bytes less than user setting,
+					((char*) p_data)[(int)Bytes-1] = ReadBytes; 	// then record received bytes at last byte in buffer
 					return ARSDC_DATA_NUM_ENOUGH;
 				}
-			}
-		}
-		else
-		return ARSDC_LSBYTE_ERR;
-		break;
-		case 2:	// 開檔允蓋寫/續寫
-		return ARSDC_WRITE_SETTING;
-		break;
-		case 4:	// 讀參
-		if( LSByte == 101 )
-		while(1)
-		{
-			if( f_readdir(&DirObj, &Finfo) != FR_OK )
-			return ARSDC_SDC_STATE_ERR;
-			if( !Finfo.fname[0] )	// 若fname為空代表已經沒有檔案可讀參
-			return ARSDC_NO_DATA_TO_READ;
-			if(Finfo.fattrib & AM_DIR)
-			;
-			else if(Finfo.fattrib & AM_RDO || Finfo.fattrib & AM_HID || Finfo.fattrib & AM_SYS)
-			;
-			else
-			{
-				sprintf(Data_p, " %5u %12s %u/%02u/%02u %02u:%02u %9lu",
-				obj_index++,	// 檔案編號
-				Finfo.fname,	// 檔案名稱
-				(Finfo.fdate >> 9) + 1980, (Finfo.fdate >> 5) & 15, Finfo.fdate & 31,	// 檔案日期
-				(Finfo.ftime >> 11), (Finfo.ftime >> 5) & 63,	// 檔案時間
-				Finfo.fsize);	// 檔案大小
+				break;
+			case LSBYTE_ATTR_TIME:
+#ifdef _DEBUG_INFO
+				printf("<temp Raw data> fno.ftime = %x\n", fno.ftime);
+#endif
+				p_data[0] = (uint8_t)(fno.ftime);
+				p_data[1] = (uint8_t)(fno.ftime >> 8);
+#ifdef _DEBUG_INFO
+				printf("<temp Raw data> p_data[0] = %x\n", p_data[0]);
+				printf("<temp Raw data> p_data[1] = %x\n", p_data[1]);
+#endif
+				break;
+			case LSBYTE_ATTR_DATE:
+#ifdef _DEBUG_INFO
+				printf("<temp Raw data> fno.fdate = %x\n", fno.fdate);
+#endif
+				p_data[0] = (uint8_t)(fno.fdate);
+				p_data[1] = (uint8_t)(fno.fdate >> 8);
+#ifdef _DEBUG_INFO
+				printf("<temp Raw data> p_data[0] = %x\n", p_data[0]);
+				printf("<temp Raw data> p_data[1] = %x\n", p_data[1]);
+#endif
+				break;
+			case LSBYTE_ATTR_ACCESS:
+				if(fno.fattrib & AM_RDO)
+					// forbidden writing
+					p_data[0] = 1;
+				else
+					p_data[1] = 0;
+				break;
+			case LSBYTE_FILE_SZ:
+				p_data[0] = fno.fsize;
+				p_data[1] = fno.fsize >> 8;
+				p_data[2] = fno.fsize >> 16;
+				p_data[3] = fno.fsize >> 24;
 				break;
 			}
-		}
-		else
-		return ARSDC_LSBYTE_ERR;
-		break;
-		case 5:	// 改名
-		return ARSDC_WRITE_SETTING;
-		break;
+
+		case 2:	// If current is at Opened file mode ([Overlap/Continue] Write mode)
+			return ARSDC_WRITE_SETTING;
+		default:
+			return ARSDC_LSBYTE_ERR;
+
 	}
 
 	ASA_SDC00_Deselect();
 
 	return 0;
+}
+
+FRESULT check_info(char* file_name, FILINFO* p_fno) {
+
+	char file_path[20];
+
+	sprintf(file_path,"/%s/%s", "ASA_DATA", file_name);
+
+	FRESULT fr;
+#ifdef _DEBUG_INFO
+	printf("Check info for '%s'...\n", file_path);
+#endif
+
+	// Check for file
+	fr = f_stat(file_path, p_fno);
+#ifdef _DEBUG_INFO
+	switch (fr) {
+		case FR_OK:
+		printf("<Raw data>fdate = %x, ftime = %x\n",p_fno->fdate, p_fno->ftime);
+		printf("Size: %lu\n", p_fno->fsize);
+		printf("Timestamp: %u/%02u/%02u, %02u:%02u\n",
+		(p_fno->fdate >> 9) + 1980, p_fno->fdate >> 5 & 15, p_fno->fdate & 31,
+		p_fno->ftime >> 11, p_fno->ftime >> 5 & 63);
+		printf("Attributes: %c%c%c%c%c\n",
+		(p_fno->fattrib & AM_DIR) ? 'D' : '-',
+		(p_fno->fattrib & AM_RDO) ? 'R' : '-',
+		(p_fno->fattrib & AM_HID) ? 'H' : '-',
+		(p_fno->fattrib & AM_SYS) ? 'S' : '-',
+		(p_fno->fattrib & AM_ARC) ? 'A' : '-');
+		break;
+
+		case FR_NO_FILE:
+		printf("It is not exist.\n");
+		break;
+
+		default:
+		printf("An error occured. (%d)\n", fr);
+	}
+#endif
+
+	return fr;
 }
--- main.c
+++ main.c
@@ -5,41 +5,101 @@ int main(void)
 	
 	ASA_M128_set();
 	printf("Test!!\n");
-	#define GetLSByteStart 100				// 輸入暫存器於記憶體列表之起始位置
-	#define SetLSByteStart 200				// 設定暫存器於記憶體列表之起始位置
-	#define FileParaSize 47				// 一行檔案參數的字串大小
-	/* Variables */
-	unsigned char ASA_ID = 1, Mask = 0xFF, Shift = 0, Setting = 0xFF, Bytes, i;
-	char check;						// 宣告模組通訊狀態變數
-	char File_Para[FileParaSize];			// 宣告檔案參數暫存字串變數
 
-	// 讀參範例
-	Bytes = FileParaSize;
+	/* Variables for ASA lib */
+	unsigned char ASA_ID = 1, Mask = 0xFF, Shift = 0, Setting = 0xFF, Bytes, i;
+
+
+	uint16_t date_rawdata;
+	uint16_t time_rawdata;
+
+	// The buffer used to swap data with SDC00 
+	uint8_t swap_buffer[64];
+	memset(swap_buffer, 0, sizeof(swap_buffer));
+
+	char check;						// module communication result state flag
+
 	while(1) {
-		// 設定為開檔讀參
-		Setting = 0x10;				// 設定變數值為允許讀取檔案參數
-		check = ASA_SDC00_set(ASA_ID, SetLSByteStart, Mask, Shift, Setting);// 送出旗標組合
+		//// Select target file
+		//ASA_SDC00_put(ASA_ID, 64, 8, "data021");
+		//ASA_SDC00_put(ASA_ID, 72, 3, "txt");
+		//
+		//// Configure to open file
+		//Setting = 0x01;				// Setting mode for openFile (readonly)
+		//check = ASA_SDC00_set(ASA_ID, 200, Mask, Shift, Setting);// 送出旗標組合
+		//if( check != 0 ) {				// 檢查回傳值做後續處理
+			//printf("Debug point 1, error code <%d>\n", check);
+			//break;
+		//}
+//
+		//unsigned char sz_buffer[4];
+		//unsigned char date_buffer[2];
+		//unsigned char time_buffer[2];
+		//// 讀取檔案參數
+		//check = ASA_SDC00_get(ASA_ID, 82, 1, sz_buffer);
+		//printf("<OUT>Sz: %ld\n", sz_buffer[0] + sz_buffer[1] >> 8 + sz_buffer[2] >> 16 + sz_buffer[3] >> 24);
+//
+		//
+		//check = ASA_SDC00_get(ASA_ID, 78, 2, date_buffer);
+		//date_rawdata = date_buffer[0];
+		//date_rawdata += date_buffer[1] << 8;
+		//printf("<OUT>Date %0x %0x   -> %0x\n", date_buffer[1], date_buffer[0], date_rawdata);
+		//printf("<OUT>Date %u/%u/%u", 1980+(uint16_t)(date_rawdata>>9), (uint16_t)(date_rawdata>>5)& 0x0F, (uint16_t)(date_rawdata)& 0x1f);
+//
+		//check = ASA_SDC00_get(ASA_ID, 76, 2, time_buffer);
+		//time_rawdata = time_buffer[0];
+		//time_rawdata += time_buffer[1] << 8;
+		//printf("<OUT>Time %0x%0x   -> %0x\n", time_buffer[1], time_buffer[0], time_rawdata);
+		//printf("<OUT>Time %u:%u:%u\n", time_rawdata>>11, (time_rawdata>>5) &0x3f, (time_rawdata & 0x1f) /2);
+		//
+		//
+		//printf("<Rec Raw data>fdate = %0x, ftime = %0x\n", date_rawdata, time_rawdata);
+//
+		///*** Read data from SDC00 ****/
+		//ASA_SDC00_get(ASA_ID, 0, 64, swap_buffer);
+		//for(int i=0; i<64; i++) {
+			//printf("%c", swap_buffer[i]);
+			//if((i+1)%8 == 0) {
+				//printf("\n");
+			//}
+		//}
+//
+		//// Configure to close file mode
+		//Setting = 0x00;
+		//check = ASA_SDC00_set(ASA_ID, 200, Mask, Shift, Setting);// 送出旗標組合
+		//if( check != 0 )				// 檢查回傳值做後續處理
+			//printf("Debug point 2, error code <%d>\n", check);
+
+
+
+		ASA_SDC00_put(ASA_ID, 64, 8, "data021");
+		ASA_SDC00_put(ASA_ID, 72, 3, "txt");
+
+		// Open file with (overlapp)
+		check = ASA_SDC00_set(ASA_ID, 200, Mask, Shift, 0x05);
 		if( check != 0 ) {				// 檢查回傳值做後續處理
-			printf("Debug point 1, error code <%d>\n", check);
+			printf("Debug point 3, error code <%d>\n", check);
+			break;
+		}
+		for(int i= 555; i<600; i++) {
+			sprintf(swap_buffer, "there is %d\n",i);
+			int sizeof_string =64;
+			for(int i=0; i<sizeof(swap_buffer); i++) {
+				if( swap_buffer[i] == '\0' ) {
+					sizeof_string = i+1;
+				}
+			}
+			ASA_SDC00_put(ASA_ID, 0, sizeof_string, swap_buffer );
+		}
+		
+		// Close file
+		check = ASA_SDC00_set(ASA_ID, 200, Mask, Shift, 0x05);
+		if( check != 0 ) {				// 檢查回傳值做後續處理
+			printf("Debug point 3, error code <%d>\n", check);
 			break;
 		}
 
-		// 讀取檔案參數
-		printf("\nFile Parameter List:\n");
-		printf("FileID __FileName__ _FileDate_ _Time _FileSize:\n");
-		while(1) {
-			check = ASA_SDC00_get(ASA_ID, GetLSByteStart+1, Bytes, File_Para);
-			if( check == 0 )			// 接收回傳值判斷參數是否讀完或異常
-			printf("%s\n", File_Para);	// 收到一行參數字串就印出一次
-			else					// check=6 表沒檔案參數可讀
-			break;				// 沒檔案參數可讀則跳離迴圈
-		}
 
-		// 設定為關檔
-		Setting = 0x01;				// 設定變數值為關檔
-		check = ASA_SDC00_set(ASA_ID, SetLSByteStart, Mask, Shift, Setting);// 送出旗標組合
-		if( check != 0 )				// 檢查回傳值做後續處理
-		printf("Debug point 2, error code <%d>\n", check);
 		break;
 	}
 }
